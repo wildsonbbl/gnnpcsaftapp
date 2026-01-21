7aad1f31438a6e1b32136f03c4fd8cbac10e07ec
wildsonbbl <wil_bbl@hotmail.com>
2026-01-21 06:33:44 -0300

    release workflow

---
+++ release-workflow.ps1
@@ -0,0 +1,15 @@
+$version='v1.0.0'
+$platform='windows'
+
+## create tag and release
+git tag $version
+git push origin $version
+gh release create -d --generate-notes --latest --verify-tag $version
+
+## create package
+uv run pyinstaller --distpath ./app_pkg/dist --workpath ./app_pkg/build --noconfirm --clean ./gnnpcsaft.spec
+cd ./app_pkg/dist/gnnpcsaft
+zip -r gnnpcsaft-$version-$platform.zip ./*
+
+## add artifact to release
+gh release upload $version gnnpcsaft-$version-$platform.zip
